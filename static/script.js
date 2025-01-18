b4d73b5e53d54b24eddaa327a7a6269f61e1cba8
iudinov <iudinov@skbkontur.ru>
2025-01-19 00:51:58 +0500

    Add static

---
+++ static/script.js
@@ -0,0 +1,10 @@
+﻿fetch('https://functions.yandexcloud.net/d4e0h2dda9g8beeu01cq')
+    .then(response => response.json())
+    .then(data => {
+        const messages = data;
+        document.body.innerHTML += "<h3 style=\"text-align: center;\"><br><br>Однако мы передаем сообщения от Вани:</h3>";
+        messages.forEach(message => {
+            document.body.innerHTML += "<div style=\"text-align: center;\"><br>" + message.text + "</div>";
+        });
+    })
+    .catch(error => console.error('Ошибка:', error));
